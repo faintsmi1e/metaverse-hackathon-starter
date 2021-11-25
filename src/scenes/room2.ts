88ae1579dfdce3613c82becd7366c5e38e3cb6e1
pinosol <zonest_64@mail.ru>
2021-11-25 19:49:53 +0300

    add timer

--- src/scenes/room2.ts
+++ src/scenes/room2.ts
@@ -3,6 +3,7 @@ import { Door } from './gameObjects/door';
 import { Timer } from './gameObjects/timer';
 import { Button } from './gameObjects/button';
 export function CreateRoom2(): void {
+  
   const door = new Door(
     new GLTFShape('models/room2/Puzzle02_Door.glb'),
     {
@@ -16,40 +17,22 @@ export function CreateRoom2(): void {
       door.openDoor();
     })
   );
-  
+
   const countdownText = new Timer({
     position: new Vector3(25.1272, 9.51119, 25.2116),
     rotation: Quaternion.Euler(-20, 180, 0),
   });
   countdownText.updateTimeString(5);
-  
 
-  const button = new Entity();
-  engine.addEntity(button);
-  button.addComponent(new GLTFShape('models/room2/Square_Button.glb'));
-  button.addComponent(
-    new Transform({
-      position: new Vector3(26.3714, 6.89, 26.8936),
-    })
-  );
-
-  // Add the button animation
-  button.addComponent(new Animator());
-  button
-    .getComponent(Animator)
-    .addClip(new AnimationState('Button_Action', { looping: false }));
-
-  // And a sound effect for when the button is pressed
-  button.addComponent(new AudioSource(new AudioClip('sounds/button.mp3')));
+  const button = new Button(new GLTFShape('models/room2/Square_Button.glb'), {
+    position: new Vector3(26.3714, 6.89, 26.8936),
+  });
 
   button.addComponent(
     new OnClick((): void => {
       if (!countdownText.hasComponent(utils.Interval)) {
-        // Animate the button press
-        button.getComponent(Animator).getClip('Button_Action').play();
-
-        // And play the button sound effect
-        button.getComponent(AudioSource).playOnce();
+        // Play the press effect
+        button.press();
 
         // Open the door
         door.openDoor();
@@ -61,8 +44,7 @@ export function CreateRoom2(): void {
             timeRemaining--;
 
             if (timeRemaining > 0) {
-              countdownText.getComponent(TextShape).value =
-                formatTimeString(timeRemaining);
+              countdownText.updateTimeString(timeRemaining);
             } else {
               // Timer has reached 0! Remove the interval to prevent a negative time
               countdownText.removeComponent(utils.Interval);
@@ -70,21 +52,11 @@ export function CreateRoom2(): void {
               // Close the door
               door.closeDoor();
 
-              countdownText.getComponent(TextShape).value = formatTimeString(5);
+              countdownText.updateTimeString(5);
             }
           })
         );
       }
     })
-  );
-}
-
-function formatTimeString(seconds: number): string {
-  const mins = Math.floor(seconds / 60);
-  const secs = Math.floor(seconds % 60);
-  return (
-    mins.toLocaleString(undefined, { minimumIntegerDigits: 2 }) +
-    ':' +
-    secs.toLocaleString(undefined, { minimumIntegerDigits: 2 })
   );
 }
